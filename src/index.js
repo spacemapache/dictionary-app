3a5beda3eda9242fb9073cf133d052c628286e0e
spacemapache <142768034+spacemapache@users.noreply.github.com>
2024-04-01 12:17:17 -0700

    added dictionary api and js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+const url = `https://api.dictionaryapi.dev/api/v2/entries/en/`;
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("btn");
 
+btn.addEventListener("click", () => {
+  let inpWord = document.getElementById("inp-word").value;
+  console.log(inpWord);
+  fetch(`${url}${inpWord}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      result.innerHTML = `
+       <div class="word">
+          <h3>${inpWord}</h3>
+          <button onclick="playSound()">
+          <i class="fa-solid fa-volume-high"></i>
+        </div>
+        <div class="wordDetails">
+          <p>${data[0].meaning[0].partOfSpeech}</p>
+          <p>${data[0].phonetic}</p>
+        </div>
+        <div class="wordMeaning">
+          <p>
+            ${data[0].meanings[0].definitions[0].defintion}
+          </p>
+          <p class="wordExample">
+            ${data[0].meanings[0].definitions[0].example || ""}
+          </p>
+    `;
+      sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+    })
+    .catch(() => {
+      result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+    });
+});
+function playSound() {
+  sound.play();
+}
 // create function for inputing word
 // create event listener for btn
 // update text to feature the correct word
-
-let btn = document.querySelector("btn");
-btn.addEventListener("click", searchWord);
-
-function searchWord(event) {
-  event.preventDefault();
-}
